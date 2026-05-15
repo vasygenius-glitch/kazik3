9cd6ed913df1bf73b98f12a8415fa06b30045e49
vasygenius-glitch <vasygenius@gmail.com>
2026-05-15 17:11:58 +0300

    Hardcode correct CREATOR_ID: 5416583030

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY_PATH", "firebase-key.json")
 CREATOR_USERNAME = os.getenv("CREATOR_USERNAME", "z_1l1")
 try:
-    CREATOR_ID = int(os.getenv("CREATOR_ID", "0"))
+    CREATOR_ID = int(os.getenv("CREATOR_ID", "5416583030"))
 except ValueError:
-    CREATOR_ID = 0
+    CREATOR_ID = 5416583030
